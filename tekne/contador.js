26168a8e26bfb46d053aa62ff3512881204a8226
Axel <axdiaz@users.noreply.github.com>
2020-03-31 17:50:34 -0300

    Add files via upload

---
+++ tekne/contador.js
@@ -0,0 +1,25 @@
+//creo una variable que se llama contador y que se inicializa con el valor 0
+var contador = 0
+
+//comandos:
+//dir = muestra las carpetas y archivos que hay dentro de la carpeta donde estoy.
+//cd "nombredelacarpetadestino" = esto sirve para meterme dentro de la carpeta elegida.
+//node "nombredelarchivodecodigo.js" = esto sirve para ejecutar el archivo  de codigo JavaScript
+//cls = sirve para limpiar la pantalla de simbolo de sistema
+
+console.log(contador)
+
+//ahora voy a sumarle 1 a la variable contador
+
+//la primer forma es la mas elemental
+contador = contador + 1
+
+console.log(contador)
+
+//la segunda forma que es reducida
+//este codigo incrementa en 1 a la variable elegida.
+contador++
+
+console.log(contador)
+
+
---
+++ tekne/contadorConFor.js
@@ -0,0 +1,9 @@
+
+var contador = 0
+
+for(var i=2 ; i<10 ; i++) {
+
+    //  contador++
+  	// console.log(contador)
+  	console.log(i/2)
+}
